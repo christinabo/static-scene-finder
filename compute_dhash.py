54f567d445e2abdc70af2cf71776382195789a02
Christina Boididou <Christina.Boididou@glasgow.ac.uk>
2018-02-26 15:30:45 +0000

    Remove unnecessary commented blocks and auxiliary list variable

--- compute_dhash.py
+++ compute_dhash.py
@@ -2,11 +2,11 @@ import dhash
 from PIL import Image
 
 
-image = Image.open('/home/cbb10g/Documents/imcd_data/lifeloggers_images/imcd_1620601/B00000840_21I7JV_20151113_101252E.JPG')
+image = Image.open('/home/cbb10g/Documents/imcd_data/lifeloggers_images/imcd_1620701/B00000682_21I71I_20150607_154657E.JPG')
 row, col = dhash.dhash_row_col(image)
 hash1 = dhash.format_hex(row, col)
 print(hash1)
-image2 = Image.open('/home/cbb10g/Documents/imcd_data/lifeloggers_images/imcd_1620601/B00000872_21I7JV_20151113_103228E.JPG')
+image2 = Image.open('/home/cbb10g/Documents/imcd_data/lifeloggers_images/imcd_1620701/B00000683_21I71I_20150607_154703E.JPG')
 row, col = dhash.dhash_row_col(image2)
 hash2 = dhash.format_hex(row, col)
 print(hash2)
